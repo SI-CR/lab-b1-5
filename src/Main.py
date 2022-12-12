8ffce85b415c9ebf3a8a30688b2a10413cfb5ee5
Héctor Alberca <hectorasq2002@gmail.com>
2022-12-12 19:26:22 +0100

    Añado grafo_ciu

--- src/Main.py
+++ src/Main.py
@@ -200,7 +200,7 @@ class XMLHandler(xml.sax.ContentHandler):
         elif heur == 2:
             heur = "Arco"
 
-        algo = Algoritmo.Algoritmo(("Algoritmo "+estrategia), pro, estrategia, grafo,heur, 200)
+        algo = Algoritmo.Algoritmo(("Algoritmo "+estrategia), pro, estrategia, grafo,heur, 1000)
 
         print(pro.name+": "+algo.nombre)
 
@@ -215,7 +215,7 @@ class XMLHandler(xml.sax.ContentHandler):
         parseador = xml.sax.make_parser()
         manejador = XMLHandler()
         parseador.setContentHandler(manejador)
-        ruta = "Grafos/nuevo.graphxml.xml"
+        ruta = "Grafos/ciu.graphML"
 
         parseador.parse(ruta)
         XMLHandler.crearMatriz(nodes, edges)
